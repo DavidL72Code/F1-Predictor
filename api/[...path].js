a4305add65139439b275a42c929ff232b6910a95
DavidL72Code <davidducle2@gmail.com>
2026-04-27 17:43:44 -0400

    fixed the return array of races

--- api/[...path].js
+++ api/[...path].js
@@ -1,6 +1,9 @@
 const { proxyToRender } = require("../lib/vercelProxy")
 
 module.exports = async (req, res) => {
-  const pathParts = Array.isArray(req.query.path) ? req.query.path : [req.query.path].filter(Boolean)
+  const queryPathParts = Array.isArray(req.query.path) ? req.query.path : [req.query.path].filter(Boolean)
+  const requestPath = (req.url || "").split("?")[0].replace(/^\/api\/?/, "")
+  const fallbackPathParts = requestPath ? requestPath.split("/").filter(Boolean) : []
+  const pathParts = queryPathParts.length > 0 ? queryPathParts : fallbackPathParts
   await proxyToRender(req, res, pathParts, ["path"])
 }
